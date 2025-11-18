1605f00849ec79a0e3a79bc3e2acc709897b7551
grupog849-netizen <grupog849@gmail.com>
2025-11-17 23:06:41 -0600

    ahhahahahhahahahahhha

--- Pagina1_IntegradorII/index.php
+++ Pagina1_IntegradorII/index.php
@@ -27,6 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['crear'])) {
         $tipo_mensaje = "error";
     }
 }
+//segunda prueba patararararrara
 
 // Procesar UPDATE
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actualizar'])) {
